32be89fbf906a401c075e8d028da7802be01f5f7
Emygut <106460135+Emygut@users.noreply.github.com>
2022-08-14 05:06:09 -0300

    Revert "Emma gutiérrez cv"

--- ValoracionFinalTSWAD-Ejercicio3Python.py
+++
@@ -1,6 +0,0 @@
-#3) Crear un lista en Python denominado "Perro" que contenga los siguientes valores:
-perro=[]
-perro= [13, "Cachorro", "12/12/2012" , "Macho", 123]
-perro[2]= "13/12/2012"
-perro[4]= 28957346
-print(perro)
