b80842ad10b80ba1ec5bbd53bf8a7d0e3b8a3989
tonylumia710 <tonylumia710@hotmail.com>
2022-08-14 16:42:12 -0300

    correción para adecuarme a la consigna

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -2,9 +2,11 @@
 # valores:  7510, 7960, 76180, 800, 4100
 # Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota # “Olivia”. 
 # Cree una función para determinar el valor mínimo de atención gastada en “Olivia”  mostrándolo en pantalla.
-
 Historial4 = ( 7510, 7960, 76180, 800, 4100 )
+def minimoTupla(tupla):
 
-print("Valor mínimo de atención de Olivia : ", min(Historial4));
+    return print("Valor mínimo de atención de Olivia : ", min(tupla));
+
+minimoTupla(Historial4);
 
 #Antonio Armando Alvarado
